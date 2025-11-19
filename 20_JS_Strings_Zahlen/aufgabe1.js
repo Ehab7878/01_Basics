a9b6e78eebb5030e166973b39e4477c691ba2ce9
Ehab <you@example.com>
2025-11-19 16:24:53 +0100

    40 If_Bedingung Video

--- 20_JS_Strings_Zahlen/aufgabe1.js
+++ 20_JS_Strings_Zahlen/aufgabe1.js
@@ -1,8 +1,9 @@
 let a = 17.4
 let result = a / 3.9
+
 console.log(result)
-// String
+
 let firstname = "Ehab"
 let lastname = "Dyab"
-console.log(firstname + " " + lastname)
 
+console.log(firstname + " " + lastname + " " + result)
