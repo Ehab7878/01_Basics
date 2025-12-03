712e79271138af0f30cf28a9802b8b2f597c48af
Ehab <you@example.com>
2025-12-03 16:00:45 +0100

    Aufgabe42_2_If_Bedingung

---
+++ Aufgabe42_2_If_Bedingung/index.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random() * 100);
+console.log(a);
+
+if (a >= 70){
+console.log("Die Zahk ist gross");
+} else if(a >= 17){
+console.log("Die Zahl ist positiv aber klein");
+} else {
+console.log("Die Zahl ist klein");
+}
