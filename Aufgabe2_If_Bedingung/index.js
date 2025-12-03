8a856efb017a488b0313145671545d2d17ace401
Ehab <you@example.com>
2025-12-03 13:55:50 +0100

    Aufgabe2_If_Bedingung

---
+++ Aufgabe2_If_Bedingung/index.js
@@ -0,0 +1,11 @@
+let colour = "red"
+
+if (colour == "green"){
+console.log("Du kannsr losfahren")
+}
+if (colour == "yellow"){
+console.log("Vorsicht")
+}
+if( colour == "red"){
+console.log("Stop!")
+}
