d700b7d750fa81ea60a85a933520e8c6c9e1e21c
Ehab <you@example.com>
2025-12-03 13:48:02 +0100

    Aufgabe1_ If_Bedingung

---
+++ Aufgabe1_If_Bedingung/index.js
@@ -0,0 +1,11 @@
+let number = 4
+
+if (number > 0){
+}
+console.log("The Number is Positiv") 
+if (number < 0){
+console.log("The number is Negativ")
+}
+if( number == 0){
+console.log("The number is zero")
+}
