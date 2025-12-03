1cbfeb1f225a2c08074ae88d0cdc4fbccc8a001e
Ehab <you@example.com>
2025-12-03 16:29:29 +0100

    Aufgabe44_If_Bedingung

---
+++ Aufgabe44_If_Bedingung/index.js
@@ -0,0 +1,13 @@
+let randomNumber = Math.floor(Math.random() * 10); 
+
+if (randomNumber < 5 ){
+console.log("The Number is less than 5");
+}
+
+if (randomNumber > 5){
+console.log("The Number is greater than 5");
+}
+
+if (randomNumber == 5){
+console.log("The Number is exactly 5");
+}
